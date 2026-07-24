2f21793a5ebc58a8d8366ab412f2ad91dbac0212
Manuel <manuel@localhost>
2026-07-24 21:02:35 +0200

    examples for exetends structs add functions to structs and pragma message to show a custom message while the source beeing compilated

---
+++ ex1.c
@@ -0,0 +1,83 @@
+#pragma message("RICORDATI: Questa sezione di codice va ottimizzata prima del rilascio!")
+#include<stdio.h>
+
+typedef struct value{
+	unsigned boo :2;
+} Val;
+
+typedef struct object Object;
+typedef struct objects Objects;
+
+struct object {
+
+	int var;
+
+	int (*func)(int);
+
+};
+
+struct objects {
+	int count;
+	Object obj;
+};
+
+//Prima struct
+struct Posizione {
+    int x;
+    int y;
+};
+
+// Seconda struct
+struct Fisica {
+    int velocita;
+    int accelerazione;
+};
+
+// Unione allo stesso livello
+struct Giocatore {
+    struct Posizione; // Struttura anonima 1
+    struct Fisica;    // Struttura anonima 2
+    int salute;       // Campo specifico del Giocatore
+};
+
+int func(int n){
+	return n * n;
+	}
+
+float media(int values[],int len){
+	
+	int sum;
+	float out;
+	for(int  x = 0 ; x < len ; x++)
+		sum += values[x];
+	
+	out = sum / len;
+	return out;	
+}
+
+
+int main(){
+	Object num ;
+	Objects o;
+
+	Val n; 
+	n.boo = 3;
+	num.var = 5;
+	o.obj.var = 7;
+	
+	num.func =  func;
+	printf("ris = %d %d -> %d %d\n",num.func(num.var),n.boo,++n.boo,o.obj.var);
+
+	float m = media((int[]){9,4,6,2,8},5);
+
+	printf("la media è %.2f \n ",m);
+
+	struct Giocatore eroe;
+
+	eroe.x = 20;
+	
+	printf("eroe posizione %d\n",eroe.x);
+	
+	return 0;	
+
+}

280025b10c6c747b827742e8d45204ee76e4fa6d
Manuel <manuel@localhost>
2026-07-26 11:04:37 +0200

    updated ex5

--- ex5.c
+++ ex5.c
@@ -1,27 +1,51 @@
 #include<stdio.h>
+#include<stdlib.h>
 
+/*
+	definisce due macro una per definire il parametro e l'altra la funzione nella struct 
+	includendo ex5.c inietto il codice che userà le macro  
+*/
 struct Gest{
-	#define MEMBER(TYPE, NAME,INIT) TYPE NAME ;
+
+	#define MEMBER(TYPE, NAME,INIT,PARAM) TYPE NAME ;
+	#define FUNC(TYPE, NAME,INIT,PARAM) TYPE (*NAME) PARAM ;
 	#include"ex5.h"
 	#undef MEMBER
+	#undef FUNC
+
 };
 
-int p(int x){
-	return x*x;
-}
 
-void init(struct Gest *g){
-	#define MEMBER(TYPE,NAME,INIT) g->NAME = INIT;
-	#include"ex5.h"
-	#undef MEMBER
-}
+/*
+	includo il file delle funzioni mi sembra più pratico che includerlo nella compilazione includo gli header una sola volta
+	da vedere le eventuali differenze 
+*/
+#include"functions.c"
 
 int main(){
+	/*
+	credo che non userei mai una struttura del genere in un progetto mi serve a studiare alcune funzionalità del codice 
+	qui dichiaro la struct per inizializzarla collego la funzione init al membro init cosi posso inizilaizzare la struttura usando la 	funzione interna
+	*/
 
-	struct Gest Tmp;
-	//Tmp.func = &p;
-	init(&Tmp);		
-	printf("%d %s\n",Tmp.id,Tmp.nome);
+	struct Gest *Tmp;
+
+	Tmp->init = &init;
+	
+	Tmp->init(Tmp);		
+	
+	//stampo la struct inizializzate
+	printf("%d %d %s\n",Tmp->func(2,0),Tmp->id,Tmp->nome);
+	
+	//modifico dei valori ...
+	Tmp->id = 20;
+
+	//...rialloco memoria e copio la stringa e...
+	Tmp = Tmp->str(Tmp, "abcdefghilmnopqrstuvz");
+	
+	//...la ristampo
+	printf("%d %d %s\n",Tmp->func(3,0),Tmp->id,Tmp->nome);
+
 	return 0;
 
 }
--- ex5.h
+++ ex5.h
@@ -1,5 +1,20 @@
+/*
+TODO 
+struttura base
+tipo, nome, val iniziale
+
+nel caso funzione
+tipo, (*nome),parametri, funzione 
+risolto creando un'altra macro per le funzioni
+# crea una stringa con il parametro passato nome -> "nome" 
+## concatena parametro passato e elemento da concatenare nome##_id -> nome_id non valido per concatenare operatori come nome##->elemento 
+*/
+
+MEMBER(int,id,10,())
+MEMBER(char*,nome,"MM",())
+FUNC(int,func,p,(int,int))
+FUNC(void,init,init,(struct Gest *))
+FUNC(int,lengthStr,lengthStr,(const char *))
+FUNC(struct Gest*,str,str,(struct Gest *,const char *))
 
 
-MEMBER(int,id,0)
-MEMBER(char*,nome,"")
-//#MEMBER(int,(*func)(int),p)

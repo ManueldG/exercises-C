4dbd2b41f31c2945ca433878324a59530d3bf46e
Manuel <manuel@localhost>
2026-08-01 22:16:32 +0200

    some changes

--- ex8.c
+++ ex8.c
@@ -7,12 +7,23 @@ int main(int argv , char **argc){
 	char x[] = {'C','i','a','o'};
 	char y[] = {48,126,'C','i','a','o',7,200,5};	
 	int i = 0;
+	char in;
+	  
 	while (y[i]>=48 && y[i]<=126){
 		write(1,&y[i],1);
 		i++;
 	}
 	y[i] = '\0';
-	printf("\n %lu %s \n",(long unsigned) y,y);	
+	printf("\n %lu %s\n \n",(long unsigned) y,y);	
+	
+	do{
+		in = getchar();
+		putchar(in);		
+		printf(" -  ");
+	}
+	while(in  != '0');
+//	fflush(stdin);
+	//memset(ptr, 0, size)
 	return 0;
 
 }
